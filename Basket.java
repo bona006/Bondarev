3ef9789505ec6f66133ccc6f5f85766f7fd6dd7b
Ilya <bona-006@mail.ru>
2024-02-12 19:04:42 +0300

    commit 2

--- Basket.java
+++ Basket.java
@@ -1,5 +1,28 @@
 package Dz1;
 
 public class Basket {
-    items
+    private Product[] items;
+
+    public Basket(Product[] items) {
+        this.items = items;
+    }
+
+    public void addProduct(Product product) {
+        this.items.push(product);
+    }
+
+    public void removeProduct(Product product) {
+    int index = this.items.indexOf(product);
+        if (index != -1) {
+            this.items.splice(index, 1);
+        }
+    }
+
+    public Product[] getItems() {
+        return items;
+    }
+
+    public void setItems(Product[] items) {
+        this.items = items;
+    }
 }
