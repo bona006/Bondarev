064ea2adc65dbb6edc18f1e9778688d178180ff8
Ilya <bona-006@mail.ru>
2024-02-12 19:19:37 +0300

    commit 3

--- Main.java
+++ Main.java
@@ -2,6 +2,21 @@ package Dz1;
 
 public class Main {
     public static void main(String[] args) {
+        Category category1 = new Category("Электроника");
+        category1.setProducts(new Product("Смартфон", 500, 4));
+        category1.setProducts(new Product( "Ноутбук", 1000, 5));
 
+        Category category2 = new Category("Одежда");
+        category2.products.push(new Product("Футболка", 20, 4.0));
+        category2.products.push(new Product("Джинсы", 50, 4.1));
+
+User user1 = new User("user1", "password1");
+User user2 = new User("user2", "password2");
+
+        user1.basket.addProduct(category1.products[0]);
+        user1.basket.addProduct(category2.products[1]);
+
+        user2.basket.addProduct(category1.products[1]);
+        user2.basket.addProduct(category2.products[0]);
         }
     }
---
+++ User.java
@@ -0,0 +1,40 @@
+package Dz1;
+
+public class User {
+    private String username;
+    private String password;
+    private Basket basket;
+
+    public User(String username, String password, Basket basket) {
+        this.username = username;
+        this.password = password;
+        this.basket = basket;
+    }
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Basket getBasket() {
+        return basket;
+    }
+
+    public void setBasket(Basket basket) {
+        this.basket = basket;
+    }
+}
